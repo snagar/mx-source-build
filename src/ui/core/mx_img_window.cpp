a545fbbbf99924c1d4b50d71111a874aa92f7b71
Saar.N <snagar.dev@protonmail.com>
2026-08-20 21:26:50 +0300

    * [LLM] Added LLM support for mission descriptions, currently available only from the User-Created Mission window. * [UI] Added a Cross Country option to the Semi Mission Creation screen. Relevant only to GA aircraft. * [LLM] To test LLM support, install a local LLM server such as LM Studio or llama.cpp. * [ImGui] Implemented the latest 'ImgWindow' library. * [UI] Fixed keyboard handling in windows after upgrading to ImGui v1.92.x. * [Windows] Hopefully fixed plugin loading failures related to Error 127, as reported in 'Log.txt'. * [Bug] Fixed an issue where the Time setting in Advanced Settings did not work correctly in the Navigation screen. The exact cause of the issue was unclear.

---
+++ src/ui/core/mx_img_window.cpp
@@ -0,0 +1,234 @@
+//
+// Created by Saar.Nagar on 2026-08-10.
+//
+#include <cassert>
+#include "mx_img_window.h"
+#include "../../../libs/imgui4xp/imgui/imgui_internal.h"
+
+namespace missionx {
+
+mx_img_window::mx_img_window(int left, int top, int right, int bottom,
+                XPLMWindowDecoration decoration, XPLMWindowLayer layer,
+                bool cursors)
+      : ImgWindow(left, top, right, bottom, decoration, layer, cursors)
+{
+  mImPlotContext = ImPlot::CreateContext();
+}
+
+
+mx_img_window::~mx_img_window()
+{
+  if (mImPlotContext) {
+    ImPlot::DestroyContext(mImPlotContext);
+  }
+}
+
+void mx_img_window::toggleWindowState()
+{
+  if (this->GetVisible())
+    this->SetVisible(false); // hide
+  else
+    this->SetVisible(true); // show
+}
+
+void mx_img_window::PushID_formatted(const char* format, ...)
+{
+  // format the variable string
+  va_list args;
+  char    sz[500];
+  va_start(args, format);
+  vsnprintf(sz, sizeof(sz), format, args);
+  va_end(args);
+  // Call the actual push function
+  ImGui::PushID(sz);
+}
+
+bool mx_img_window::ButtonTooltip(const char* label, const char* tip, ImU32 colFg, ImU32 colBg, const ImVec2& size)
+{
+  // Setup colors
+  if (colFg != IM_COL32(1, 1, 1, 0))
+    ImGui::PushStyleColor(ImGuiCol_Text, colFg);
+  if (colBg != IM_COL32(1, 1, 1, 0))
+    ImGui::PushStyleColor(ImGuiCol_Button, colBg);
+
+  // do the button
+  bool b = ImGui::Button(label, size);
+
+  // restore previous colors
+  if (colBg != IM_COL32(1, 1, 1, 0))
+    ImGui::PopStyleColor();
+  if (colFg != IM_COL32(1, 1, 1, 0))
+    ImGui::PopStyleColor();
+
+  // do the tooltip
+  if (tip && ImGui::IsItemHovered())
+    ImGui::SetTooltip("%s", tip);
+
+  // return if button pressed
+  return b;
+}
+
+ImVec4 mx_img_window::mx_get_color_as_im_vec4(const std::string& inColor_s)
+{
+  if ( missionx::mxconst::get_YELLOW() == inColor_s)
+    return (ImVec4(1.0f, 1.0f, 0.0f, 1.0));
+  else if (missionx::mxconst::get_WHITE() == inColor_s)
+    return (ImVec4(1.0f, 1.0f, 1.0f, 1.0));
+  else if (missionx::mxconst::get_GREEN() == inColor_s)
+    return (ImVec4(0.0f, 1.0f, 0.0f, 1.0));
+  else if (missionx::mxconst::get_RED() == inColor_s)
+    return (ImVec4(1.0f, 0.0f, 0.0f, 1.0));
+  else if (missionx::mxconst::get_BLUE() == inColor_s)
+    return (ImVec4(0.0f, 0.0f, 1.0f, 1.0));
+  else if (missionx::mxconst::get_ORANGE() == inColor_s)
+    return (ImVec4(1.0f, 0.5f, 0.0f, 1.0));
+  else if (missionx::mxconst::get_PURPLE() == inColor_s)
+    return (ImVec4(1.0f, 0.0f, 1.0f, 1.0));
+  else if (missionx::mxconst::get_BLACK() == inColor_s)
+    return (ImVec4(0.0f, 0.0f, 0.0f, 1.0));
+
+  return ImVec4(1.0f, 1.0f, 1.0f, 1.0); // white
+}
+
+void mx_img_window::HelpMarker(const char* desc, ImVec4 inTextColor)
+{
+  ImGui::TextDisabled("(?)");
+  if (ImGui::IsItemHovered())
+  {
+    ImGui::PushStyleColor(ImGuiCol_Text, inTextColor);
+
+    ImGui::BeginTooltip();
+    ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+    ImGui::TextUnformatted(desc);
+    ImGui::PopTextWrapPos();
+    ImGui::EndTooltip();
+
+    ImGui::PopStyleColor();
+  }
+}
+
+void mx_img_window::mxUiHelpMarker(ImVec4 inTextColor, const char* desc)
+{
+  HelpMarker(desc, inTextColor);
+}
+
+void mx_img_window::mx_add_tooltip(ImVec4 inColor, const std::string& inTip) const
+{
+  if (!IsInVR() && ImGui::IsItemHovered())
+  {
+    ImGui::PushStyleColor(ImGuiCol_Text, inColor); // dark gray
+
+    ImGui::BeginTooltip();
+    // ImGui::PushItemWidth(200.0f);
+    ImGui::Text("%s", inTip.c_str());
+    // ImGui::PopItemWidth();
+    ImGui::EndTooltip();
+
+    ImGui::PopStyleColor(1);
+  }
+}
+
+ImVec4 mx_img_window::mxConvertMxVec4ToImVec4(const missionx::mxVec4& inMxVec4)
+{
+  return ImVec4(inMxVec4.x, inMxVec4.y, inMxVec4.z, inMxVec4.w);
+}
+
+float mx_img_window::mxUiGetContentWidth()
+{
+  ImGuiWindow* window = GImGui->CurrentWindow;
+  return window->ContentRegionRect.GetWidth();
+}
+
+float mx_img_window::mxUiGetContentHeight()
+{
+  ImGuiWindow* window = GImGui->CurrentWindow;
+  return window->ContentRegionRect.GetHeight();
+}
+
+ImVec2 mx_img_window::mxUiGetWindowContentWxH()
+{
+  return ImVec2(mxUiGetContentWidth(), mxUiGetContentHeight());
+}
+
+void mx_img_window::mxUiSetDefaultFont()
+{
+  this->mxUiResetAllFontsToDefault();
+}
+
+void mx_img_window::mxUiResetAllFontsToDefault()
+{
+  while (this->iFontQueue--)
+    ImGui::PopFont();
+
+  this->iFontQueue = 0;
+}
+
+void mx_img_window::mxUiSetFont(const std::string& inTextType)
+{
+  assert(!missionx::MxUICore::mapFontTypeMeta.empty() && "No fonts were loaded.");
+
+  // read font meta data for inTextType. Holds Font size.
+  if (missionx::MxUICore::mapFontTypeMeta.contains(inTextType))
+  {
+    const auto ftm = missionx::MxUICore::mapFontTypeMeta[inTextType];
+    missionx::MxUICore::mapFontTypesBeingUsedInProgram[inTextType] = ftm.id;
+
+    ImGui::PushFont(ImgWindow::sFontAtlas->getAtlas()->Fonts[ftm.id], ftm.fSizePx);
+    ++this->iFontQueue;
+  }
+}
+
+void mx_img_window::mxUiReleaseLastFont(const int inHowManyCycles)
+{
+  for (int loop1 = 0; loop1 < inHowManyCycles; ++loop1)
+  {
+    if (this->iFontQueue > 0)
+    {
+      ImGui::PopFont();
+      this->iFontQueue--;
+    }
+  }
+}
+
+bool mx_img_window::mxStartUiDisableState(const bool in_true_exp_to_disable)
+{
+  if (!in_true_exp_to_disable) // if expression is false, skip.
+    return in_true_exp_to_disable;
+
+  ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
+  ImGui::PushStyleVar(ImGuiStyleVar_Alpha, ImGui::GetStyle().Alpha * 0.5f);
+
+  return in_true_exp_to_disable;
+}
+
+void mx_img_window::mxEndUiDisableState(const bool in_true_exp_to_disable)
+{
+  if (! in_true_exp_to_disable)
+    return;
+
+  ImGui::PopItemFlag();
+  ImGui::PopStyleVar();
+}
+
+bool mx_img_window::mxUiButtonTooltip(const char* label, const char* tip, ImVec4 colFg, ImVec4 colBg, const ImVec2& size)
+{
+  // Setup colors
+  ImGui::PushStyleColor(ImGuiCol_Text, colFg);
+  ImGui::PushStyleColor(ImGuiCol_Button, colBg);
+
+  // do the button
+  bool b = ImGui::Button(label, size);
+
+  // restore previous colors
+  ImGui::PopStyleColor();
+  ImGui::PopStyleColor();
+
+  // do the tooltip
+  if (tip && ImGui::IsItemHovered())
+    ImGui::SetTooltip("%s", tip);
+
+  // return if button pressed
+  return b;
+}
+
+} // missionx namespace
---
+++ src/ui/core/mx_img_window.h
@@ -0,0 +1,82 @@
+//
+// Created by Saar.Nagar on 2026-08-10.
+//
+
+#ifndef MISSIONX_MX_IMG_WINDOW_H
+#define MISSIONX_MX_IMG_WINDOW_H
+#include <queue>
+
+#include "../../../libs/imgui4xp/ImgWindow/ImgWindow.h"
+#include "mx_colors.hpp"
+#include "../../../libs/imgui4xp/imgui/implot/implot.h" // v3.0.255.1
+#include "../MxUICore.hpp" // v3.303.14 holds Font related data
+#include "../../core/xx_mission_constants.hpp" // v3.303.14 holds Font related data
+
+
+
+namespace missionx {
+
+class mx_img_window : public ImgWindow {
+private:
+  int            iFontQueue{ 0 };
+  // ImPlotContext* mImPlotContext = ImPlot::CreateContext();
+  ImPlotContext* mImPlotContext = {nullptr};
+public:
+  // Inherits all constructors from ImgWindow automatically
+  // using ImgWindow::ImgWindow;
+
+  mx_img_window(int left, int top, int right, int bottom,
+                  XPLMWindowDecoration decoration, XPLMWindowLayer layer,
+                  bool cursors = true);
+
+  ~mx_img_window();
+
+  // Extend ImgWindow
+  ImVec2 vec2_plus(ImVec2 v1, ImVec2 v2) { return ImVec2(v1.x + v2.x, v1.y + v1.y); };
+  ImVec2 vec2_minus(ImVec2 v1, ImVec2 v2) { return ImVec2(v1.x - v2.x, v1.y - v1.y); };
+  ImVec2 vec2_multi(ImVec2 v1, ImVec2 v2) { return ImVec2(v1.x * v2.x, v1.y * v1.y); };
+  ImVec2 vec2_multi_num(ImVec2 v1, float fVal) { return ImVec2(v1.x * fVal, v1.y * fVal); };
+
+  void toggleWindowState();
+  // These functions where originally in "starter window" and in their oen namespace.
+  // I don't see the reason not to move them into the base window class.
+  // @brief Helper for creating unique IDs
+  // @details Required when creating many widgets in a loop, e.g. in a table
+  void PushID_formatted(const char* format, ...); // v3.303.14
+
+  bool ButtonTooltip(const char* label,
+        const char* tip = nullptr,
+        ImU32 colFg = IM_COL32(1, 1, 1, 0),
+        ImU32 colBg = IM_COL32(1, 1, 1, 0),
+        const ImVec2& size = ImVec2(0, 0));
+
+  static ImVec4 mx_get_color_as_im_vec4(const std::string& inColor_s); // used with mx_pad messages color
+
+  static void HelpMarker(const char* desc, ImVec4 inTextColor = missionx::color::color_vec4_white); // from IMGUI demo // v3.303.14 added default color
+  static void mxUiHelpMarker(ImVec4 inTextColor,const char* desc); // saar, missionx. Prefer color first
+
+  void mx_add_tooltip(ImVec4 inColor/* = missionx::color::color_vec4_white*/, const std::string& inTip) const;
+
+  ImVec4 mxConvertMxVec4ToImVec4(const missionx::mxVec4 & inMxVec4 ); // the function replaces a deprecated ImGui::GetWindowContentRegionWidth(). The new replacer functions does not seem to do the trick in some cases, so I have copied the logic of the original function into my own one in the hope it will continue and server me.
+  float mxUiGetContentWidth(); // the function replaces a deprecated ImGui::GetWindowContentRegionWidth(). The new replacer functions does not seem to do the trick in some cases, so I have copied the logic of the original function into my own one in the hope it will continue and server me.
+  float mxUiGetContentHeight(); //
+  ImVec2 mxUiGetWindowContentWxH(); //
+
+  void mxUiSetDefaultFont();
+  void mxUiResetAllFontsToDefault();
+
+  void mxUiSetFont(const std::string& inTextType); // set the font using the font type name liked TEXT_TYPE_TITLE_REG, TEXT_TYPE_TITLE_BIG, TEXT_TYPE_TEXT_REG
+  void mxUiReleaseLastFont(int inHowManyCycles = 1); // pop out Fonts we pushed, default is only the last one but you can release more than one. Updates iFontQueue.
+
+  bool mxStartUiDisableState(bool in_true_exp_to_disable); // v24.02.6 true means not disable
+  void mxEndUiDisableState(bool in_true_exp_to_disable);   // v24.02.6 true means not disable
+
+  bool mxUiButtonTooltip(const char* label, const char* tip = nullptr, ImVec4 colFg = ImVec4(1.0f, 1.0f, 1.0f, 1.0f), ImVec4 colBg = ImVec4(1.0f, 1.0f, 1.0f, 1.0f), const ImVec2& size = ImVec2(0, 0));
+
+
+
+};
+
+} // missionx
+
+#endif //MISSIONX_MX_IMG_WINDOW_H
