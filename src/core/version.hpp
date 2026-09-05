339334157958e123f7df56d1194ff831cd7562ec
Saar.N <snagar.dev@protonmail.com>
2026-09-05 12:59:34 +0300

    . [ui] Better llm option visibility dependent on the picked semi activity. . [llm] Continue optimization of the instructions to the llm. . [ui] Better support for special UTF characters that also present in ascii table.

--- src/core/version.hpp
+++
@@ -1,2 +0,0 @@
-// The version file will be modified by the auto build scripts in GitHub and will be used to dynamicaly display the type of the build, based on the tags we create.
-#define SPECIAL_BUILD ""
